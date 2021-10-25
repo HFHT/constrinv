126549b284b83ec69d83a7166d9971ea2686ec2a
HFHT <admin@habitattucson.org>
2021-10-25 12:46:48 -0700

    image upload debugging

--- src/services/ImageUpload.js
+++ src/services/ImageUpload.js
@@ -1,20 +1,22 @@
 // Azure serverless functions are utilized to access the MongoDB
 const { BlockBlobClient, AnonymousCredential } = require("@azure/storage-blob");
 
-export async function ImageUpload( file ) {
-// for now get a new sas Key for each upload, later we can add expiry check
-console.log('ImageParm:',file)
-    const {url, sasKey} = await (await fetch(`${process.env.REACT_APP_AZURE_FUNC_URL}/api/HFHTSasToken`)).json();
-    console.log('Sas key:',url, sasKey)
+export async function ImageUpload(file) {
+    // for now get a new sas Key for each upload, later we can add expiry check
+    console.log('ImageParm:', file)
+    const { url, sasKey } = await (await fetch(`${process.env.REACT_APP_AZURE_FUNC_URL}/api/HFHTSasToken`)).json();
+    console.log('Sas key:', url, sasKey)
     const container = 'images'
     var blobName = file.name
-    var login = `${url}/${container}/${blobName}?${sasKey}`; 
+    var login = `${url}/${container}/${blobName}?${sasKey}`;
     var blockBlobClient = new BlockBlobClient(login, new AnonymousCredential)
     const reader = new FileReader()
     reader.onload = () => {
-        blockBlobClient.uploadBrowserData(reader.result);
+        console.log('Reader result', reader.result)
+        console.log('Reader', reader)
+        blockBlobClient.uploadData(reader.result);
     }
     reader.onabort = () => console.log('file reading was aborted')
-    reader.onerror = () => console.log('file reading has failed')    
+    reader.onerror = () => console.log('file reading has failed')
     reader.readAsArrayBuffer(file)
 }
